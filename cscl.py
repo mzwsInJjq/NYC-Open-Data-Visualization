7304e976a4781358b05647b8d817e236cd5de593
J. Chen <140699748+mzwsInJjq@users.noreply.github.com>
2025-03-25 22:13:11 +0800

    Add files via upload

---
+++ cscl.py
@@ -0,0 +1,18 @@
+# NYC Street Centerline (CSCL)
+# https://data.cityofnewyork.us/City-Government/NYC-Street-Centerline-CSCL-/inkn-q76z/about_data
+
+import pandas as pd
+import geopandas as gpd
+import geodatasets
+import matplotlib.pyplot as plt
+
+cscl = gpd.read_file(r"C:\Users\Chen\Downloads\NYC Street Centerline (CSCL)_20250325.geojson")
+cscl = cscl[cscl['rw_type'] != '14']
+nybb = gpd.read_file(geodatasets.get_path("nybb"))
+nybb = nybb.to_crs(epsg=4326)
+
+fig, ax = plt.subplots(figsize=(20, 20))
+cscl.plot(ax=ax, color='k', label='CSCL', linewidth=0.5)
+nybb.plot(ax=ax, color='b', label='NYBB', alpha=0.5)
+
+plt.savefig('cscl.pdf', format='pdf', bbox_inches='tight')
